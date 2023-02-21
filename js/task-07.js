a9510541f70ce4cd5a207213de77dcb17564f5b4
VladyslavYakivets <aquartis97@gmail.com>
2023-02-21 22:51:44 +0200

    Домашня робота

---
+++ js/task-07.js
@@ -0,0 +1,4 @@
+const input = document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
+
+input.addEventListener('input', event => text.style.fontSize = Number(input.value) + 'px');
